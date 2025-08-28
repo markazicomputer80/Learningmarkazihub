94a72255fa4b340689029bcecb645f66f1a02b55
ملوحیان <markazicomputer80@gmail.com>
2025-08-28 20:50:28 +0330

    Add files via upload

---
+++ WebProject/fastfood/index.php
@@ -0,0 +1,83 @@
+﻿<?php
+  include_once('include/connection.php');
+  include_once('include/function.php');
+  echo '<link rel="stylesheet" type="text/css" href="style/style.css"/>';
+  echo '<div id="container" align="center">';
+?>
+  <form action="index.php" method="post" align="center" >
+     Pish ghaza <input type="radio" value="1" name="type"/>
+     Main course <input type="radio" value="2" name="type"/>
+     Desert <input type="radio" value="3" name="type"/>
+
+     <input type="submit" value="نمایش" />
+  </form>
+<?php
+  echo '<div id="header"></div>';
+   if($conn==false)
+   {
+      DisplayErrors();
+      die();
+   }
+   else
+   {
+    if(isset($_POST['type']))
+    {
+    $type=$_POST['type'];
+    switch($type)
+    {
+    case "1": // پیش غذا
+    {
+       $h= "پیش غذا";
+       $div='<div id="starter">';
+       break;
+    }
+    case "2": //غذای اصلی
+    {
+      $h= "غذای اصلی";
+      $div='<div id="main">';
+      break;
+    }
+    case "3": // دسر
+    {
+      $h= "دسر";
+      $div='<div id="starter">';
+      break;
+    }
+    }  // switch
+    $sql="SELECT FoodInfo.*, FoodMaterial.Material
+          FROM  FoodInfo INNER JOIN
+          FoodMaterial ON FoodInfo.Foodcode = FoodMaterial.FoodCode  where type='$type' ";
+
+     $stmt=sqlsrv_query($conn,$sql); //or die ('Query Error'.sqlsrv_error());
+
+   //---------------------------------//
+   echo $div;
+   echo '<table dir="rtl" cellspacing="5px" border="1">';
+   echo '<tr>';
+     echo '<th>'; echo  $h;    echo '</th>' ;
+     echo '<th>'; echo 'نام';  echo '</th> ';
+     echo '<th>' ; echo 'مواد اولیه'; echo '</th>';
+     echo '<th>';  echo 'قیمت'; echo '</th>';
+   echo '</tr>';
+
+ while( $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) )
+   {
+     echo ('<tr>');
+     echo('<td>');
+?>
+     <img src="img/<?php echo $row['pic']; ?>" />
+<?php
+     echo('</td>');
+     echo('<td>'.$row['FoodTitle'].'</td>');
+     echo('<td>'.$row['Material'].'</td>') ;              // این فیلد متریال است که باید نمایش داده شود
+     echo('<td>'.$row['Price'].'</td>');
+     echo('</tr>');
+   }  // End of While
+   echo('</div>');
+   echo('</table>');
+   echo('<hr>');
+  } // if
+ }// esle
+?>
+
+
---
+++ WebProject/fastfood/insert.php
@@ -0,0 +1,33 @@
+﻿<?php
+// insert.php   
+$serverName = "pc0\muser";  /* معرفی متغیر سرور*/
+$connectionInfo = array( "Database"=>"fastfood", "CharacterSet" => "UTF-8"); /* معرفی متغیر بانک اطلاعاتی و کدینگ فارسی*/
+$conn = sqlsrv_connect( $serverName, $connectionInfo); /* اجرای دستور کوئری فوق*/
+/* در صورتیکه ارتباط با بانک اطلاعاتی برقرار نشد پیام خطای زیر نمایش داده میشود*/
+if( !$conn ){
+     echo "اتصال برقرار نشد.<br />";
+     die( print_r( sqlsrv_errors(), true));
+   }
+
+//-Insert into FoodInfo---------------------------------------------------------------------------------   
+ /* درصورتیکه اطلاعات فیلدهای صفحه اچ تی ام ال خالی باشد به صفحه اصلی ورود اطلاعات غذا برمیگردد*/  
+if($_POST['title']=='' || $_POST['type']=='' || $_POST['material']=='' || $_POST['price']=='' || $_POST['pic']=='')
+{ header("Location:insert.html");}
+/* اطلاعات نام - نوع - قیمت - عکس - در این فایل ثبت میگردد*/
+$sql="insert into FoodInfo([type],[Price],[FoodTitle],[pic]) values(?,?,?,?)";
+$params=array($_POST['type'],$_POST['price'],$_POST['title'],$_POST['pic']);
+$stmt=sqlsrv_query($conn,$sql,$params); // Go
+
+//-Insert Into Food material-----------------------------------------------------------------------------
+/* برای پیدا کردن آخرین ردیف فودکد دستور زیر اجرا میگردد */
+/* این متغیر برای درج در متریال اینفو لازم میباشد*/
+$sql="select max(Foodcode) as mcode from FoodInfo";
+$materialstmt=sqlsrv_query($conn,$sql); /* اجرای دستور کوئری فوق*/
+$FCode = sqlsrv_fetch_array( $materialstmt, SQLSRV_FETCH_ASSOC);
+
+/* اطلاعات مواد اولیه بر اساس فودکد در این فایل ثبت میگردد*/
+$sql="insert into FoodMaterial(FoodCode,Material) values(?,?)";
+$params=array($FCode['mcode'],$_POST['material']); /* واگذاری اطلاعات  ثبت شده در سایت به متغییر ها با پارامتر  */
+$stmt=sqlsrv_query($conn,$sql,$params);/* اجرای دستور کوئری فوق*/
+header("Location:insert.html");
+?>
